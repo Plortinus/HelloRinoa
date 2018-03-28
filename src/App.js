8c0b81009e02bcadea1389e8c9dbbfbe55c87d33
王晓勇 <plortinus@gmail.com>
2018-03-28 17:25:48 +0800

    currentColor IE11开始支持

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './cat.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello Rinoa</h1>
-        </header>
-        <p className="App-intro">
-          你好,婉转喵!
-        </p>
+        <div className="button-wrap">
+          <button className="rinoa">你好,婉转喵!</button>
+        </div>
       </div>
     );
   }
