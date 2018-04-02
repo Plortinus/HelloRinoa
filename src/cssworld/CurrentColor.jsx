dcdfccae618c17b40de48951be168afcd3cb2d84
王晓勇 <plortinus@gmail.com>
2018-04-02 17:54:53 +0800

    css3 current color

--- src/cssworld/CurrentColor.jsx
+++ src/cssworld/CurrentColor.jsx
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react'
 import './CurrentColor.css'
 
-class CurrentColor extends Component {
-  render() {
+class HelloRinoa extends React.Component {
+  constructor(props) {
+    super(props)
+    this.changeColor = this.changeColor.bind(this)
+    this.state = {
+      color: '#333'
+    }
+  }
+  changeColor() {
+    // 随机color
+    const color = '#' + (Math.random() * 0xffffff << 0).toString(16)
+    this.setState({ color })
+  }
+  render () {
     return (
-      <div className="r">
-        <button className="rinoa">你好,婉转喵!</button>
+      <div className="child" style={{ color: this.state.color }}>
+        <button style={{ color: this.state.color }} className="rinoa" onClick={this.changeColor}>你好,婉转喵!{this.props.number}</button>
       </div>
-    );
+    )
   }
 }
 
-export default CurrentColor;
+class CurrentColor extends React.Component {
+  render() {
+    // jsx列表渲染
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+    const listItems = numbers.map((number, index) => <HelloRinoa key={index} number={number} />)
+    return (
+      <div className="parent">
+        {listItems}
+      </div>
+    )
+  }
+}
+
+export default CurrentColor
